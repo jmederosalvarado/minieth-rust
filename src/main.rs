6121e3c523063bf321fcd1259c17cbc1c86ba9b1
Jorge Mederos Alvarado <jmederosalvarado@gmail.com>
2021-10-05 15:33:52 -0400

    feat: add transaction receiving code

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,56 @@
 #![allow(dead_code)]
+#![allow(unused_variables)]
 
-use std::time::SystemTime;
+use serde::{Deserialize, Serialize};
+use std::io::prelude::*;
+use std::net::TcpListener;
+use std::thread;
 
+#[derive(Serialize, Deserialize, Debug)]
 struct Block {
-    index: u32,
+    nonce: u32,
     timestamp: f32,
     transactions: Vec<Transaction>,
 }
 
-impl Block {
-    fn new(index: u32) -> Self {
-        Self {
-            index,
-            timestamp: SystemTime::now()
-                .duration_since(SystemTime::UNIX_EPOCH)
-                .unwrap()
-                .as_secs_f32(),
-            transactions: vec![],
-        }
-    }
+#[derive(Serialize, Deserialize, Debug)]
+struct Transaction {
+    nonce: u32,
+    gas_price: u32,
+    gas_limit: u32,
+    to: TransactionTo,
+    value: u32,
+    // TODO: This should actually be v, r, s
+    // Yellow Paper: Page 4 and Appendix F
+    sender: u32,
+}
 
-    fn add_transaction(&mut self, transaction: Transaction) {
-        self.transactions.push(transaction)
+#[derive(Serialize, Deserialize, Debug)]
+enum TransactionTo {
+    Address(String),
+    Empty,
+}
+
+fn listen_transactions(host: &str, port: &str) {
+    let listener = TcpListener::bind(format!("{}:{}", host, port)).unwrap();
+
+    for stream in listener.incoming() {
+        let mut buffer = String::new();
+        stream.unwrap().read_to_string(&mut buffer).unwrap();
+        let transaction: Transaction = serde_json::from_str(&buffer).unwrap();
+        spread_transaction(&transaction);
+        process_transaction(transaction);
     }
 }
 
-struct Transaction {
-    sender: u32,
-    recipient: u32,
-    amount: u32,
+fn spread_transaction(transaction: &Transaction) {}
+
+fn process_transaction(transaction: Transaction) {
+    println!("Processing transaction {:?}", transaction);
 }
 
 fn main() {
-    println!("Hello, world!");
+    let chain: Vec<Block>;
+    let transactions_thread = thread::spawn(|| listen_transactions("0.0.0.0", "8000"));
+    transactions_thread.join().unwrap();
 }
