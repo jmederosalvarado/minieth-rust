5c3724ff2903d35eb9c3a7e5bb8487e056f68849
Jorge Mederos Alvarado <jmederosalvarado@gmail.com>
2021-10-05 01:09:39 -0400

    feat: add basic block structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+#![allow(dead_code)]
+
+use std::time::SystemTime;
+
+struct Block {
+    index: u32,
+    timestamp: f32,
+    transactions: Vec<Transaction>,
+}
+
+impl Block {
+    fn new(index: u32) -> Self {
+        Self {
+            index,
+            timestamp: SystemTime::now()
+                .duration_since(SystemTime::UNIX_EPOCH)
+                .unwrap()
+                .as_secs_f32(),
+            transactions: vec![],
+        }
+    }
+
+    fn add_transaction(&mut self, transaction: Transaction) {
+        self.transactions.push(transaction)
+    }
+}
+
+struct Transaction {
+    sender: u32,
+    recipient: u32,
+    amount: u32,
+}
+
 fn main() {
     println!("Hello, world!");
 }
